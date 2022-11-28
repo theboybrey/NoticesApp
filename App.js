e402eae7d18ed9a147ef8c211239f44269c3d58f
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-11-28 02:42:18 -0800

    LandingPage added

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 
+const Stack = createStackNavigator(); 
 
 export default function App() {
   return (
-    <NavigationContainer>{/* Rest of your app code */}
+    <NavigationContainer>
 
     </NavigationContainer>
 
--- src/screens/LandingPage.js
+++ src/screens/LandingPage.js
@@ -1,14 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const index = () => {
+const LandingPage = () => {
   return (
     <View>
-      <Text>index</Text>
+      <Text>LandingPage</Text>
     </View>
   )
 }
 
-export default index
+export default LandingPage
 
 const styles = StyleSheet.create({})
