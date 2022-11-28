792dd0c956340c9f3dca8eacc9450dcd1a0ebf65
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-11-28 03:14:45 -0800

    Creating inheritence from the Component

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -1,12 +1,14 @@
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native'
 import React from 'react'
 
-const Register = () => {
-  return (
-    <View>
-      <Text>Register</Text>
-    </View>
-  )
+export class Register extends Component {
+    render() {
+        return (
+            <View>
+
+            </View>
+        )
+    }
 }
 
 export default Register
